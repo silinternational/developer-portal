048b173b3e3bdb5c7cfd9356edc889d69094f26a
Matt Henderson <matt_henderson@sil.org>
2016-06-07 09:52:32 -0400

    Add basic test class for CostScheme model.

---
+++ application/protected/tests/unit/CostSchemeTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Sil\DevPortal\tests\unit;
+
+class CostSchemeTest extends \CDbTestCase
+{
+    public $fixtures = array(
+        'costSchemes' => 'CostScheme',
+    );
+    
+    public function testHasAtLeastOnePrice()
+    {
+        $this->markTestIncomplete('Test not yet written.');
+    }
+    
+    public function testHasBothOrNeither()
+    {
+        $this->markTestIncomplete('Test not yet written.');
+    }
+}
