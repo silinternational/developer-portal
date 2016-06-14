500ab38422ad1a76c887826557fe01cecf347b8e
Matt Henderson <matt_henderson@sil.org>
2016-06-14 16:59:45 -0400

    Delete accidental cost_scheme fixture record.

--- application/protected/tests/fixtures/cost_scheme.php
+++ application/protected/tests/fixtures/cost_scheme.php
@@ -1,11 +1,3 @@
 <?php
 return array(
-    array(
-        'api_visibility_domain_id' => 1,
-        'api_id' => 17,
-        'domain' => 'invited-domain.example.com',
-        'invited_by_user_id' => 17,
-        'created' => '2016-06-07 14:18:40',
-        'updated' => '2016-06-07 14:18:40',
-    ),
 );
