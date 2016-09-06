988a160a6312655cae91576898177a2cff940824
Matt Henderson <matt_henderson@sil.org>
2016-09-06 15:26:30 -0400

    Add lower sections to new home page.

--- application/protected/views/site/index.php
+++ application/protected/views/site/index.php
@@ -5,15 +5,15 @@ $this->pageTitle = 'Welcome';
 ?>
 <div class="home-page">
     <div class="hero-unit">
-        <div class="row">
-            <div class="span7">
+        <div class="row-fluid">
+            <div class="span8">
                 <h2>
                     <span class="text-sm">Welcome to the </span><br />
                     <span class="sil-blue site-name"><?= \CHtml::encode(Yii::app()->name); ?></span>
                 </h2>
             </div>
             
-            <div class="span3">
+            <div class="span4">
                 <div id="get-started">
                     <h2>Get Started</h2>
                     Login using ___<br />
@@ -23,27 +23,29 @@ $this->pageTitle = 'Welcome';
         </div>
     </div>
     
-    <div class="row">
-        <div class="span4">
-            <h2>API Owners</h2>
-            <p>
-                Publish your APIs and manage access to who can use them.
-            </p>
-        </div>
+    <div class="pad-horiz-extra">
+        <div class="row-fluid">
+            <div class="span8">
+                <h2>Intro</h2>
+                <p>
+                    Lorem ipsum...
+                </p>
+            </div>
 
-        <div class="span4">
-            <h2>API Consumers</h2>
-            <p>
-                Discover new APIs, read integration documentation, and request access
-                to APIs.
-            </p>
-        </div>
-
-        <div class="span4">
-            <h2>Getting Started</h2>
-            <p>
-                To begin, click the Login button above.
-            </p>
+            <div class="span4">
+                <h2>Popular APIs</h2>
+                <dl>
+                    <dt><a href="#">One</a></dt>
+                    <dd>A sample API</dd>
+                    
+                    <dt><a href="#">Two</a></dt>
+                    <dd>Another sample API</dd>
+                    
+                    <dt><a href="#">Three</a></dt>
+                    <dd>A third sample API</dd>
+                </dl>
+                <a href="#" class="pull-right space-after-icon"><i class="icon-arrow-right"></i>Browse APIs</a>
+            </div>
         </div>
     </div>
 </div>
