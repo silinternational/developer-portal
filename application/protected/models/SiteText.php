5b48e0cc56f1c4c4e11056ae561d3c75eb308683
Matt Henderson <matt_henderson@sil.org>
2016-09-13 15:46:42 -0400

    Save SiteText HTML to view file.

--- application/protected/models/SiteText.php
+++ application/protected/models/SiteText.php
@@ -9,6 +9,8 @@ class SiteText extends SiteTextBase
     {
         parent::afterSave();
         
+        $this->saveHtmlToFile();
+        
         /** @todo Generate the HTML from this Markdown content and save it to the applicable partial view file. */
     }
     
@@ -38,6 +40,31 @@ class SiteText extends SiteTextBase
                 'message' => 'Please use only lowercase letters (a-z) and hyphens (-) in the name.',
             ),
         ), parent::rules());
+    }
+    
+    protected function saveHtmlToFile()
+    {
+        $reSanitizedName = $this->slugify($this->name);
+        if ($reSanitizedName !== $this->name) {
+            throw new \Exception('Invalid site text name: ' . $this->name, 1473778429);
+        }
+        
+        $markdownParser = new \CMarkdownParser();
+        $renderedHtml = $markdownParser->safeTransform($this->markdown_content);
+        
+        $filePath = sprintf(
+            '%s/../views/partials/site-text/%s.php',
+            __DIR__,
+            $reSanitizedName
+        );
+        $result = file_put_contents($filePath, $renderedHtml);
+        if ($result === false) {
+            throw new \Exception(
+                'Failed to save the HTML generated from this Site Text\'s '
+                . 'markdown content to the appropriate view file.',
+                1473778699
+            );
+        }
     }
     
     public function slugify($name)
