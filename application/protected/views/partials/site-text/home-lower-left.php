3a20c411c0f5be34eadc3babe5cfecd1223def72
Matt Henderson <matt_henderson@sil.org>
2016-09-13 08:44:54 -0400

    Empty "template" view files.

--- application/protected/views/partials/site-text/home-lower-left.php
+++
@@ -1,5 +0,0 @@
-<?php /* @var $this Controller */ ?>
-<h2>Intro</h2>
-<p>
-    Lorem ipsum dolor sit amet...
-</p>
--- application/protected/views/partials/site-text/home-lower-right.php
+++
@@ -1,5 +0,0 @@
-<?php /* @var $this Controller */ ?>
-<h2>Sample</h2>
-<p>
-    Other sample content...
-</p>
