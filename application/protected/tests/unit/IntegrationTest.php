3cf2559404fa032e0615868afcf22164d7e23906
Matt Henderson <matt_henderson@sil.org>
2016-11-02 11:57:06 -0400

    Add call to system-check URL in unit tests.

---
+++ application/protected/tests/unit/IntegrationTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Sil\DevPortal\tests\unit;
+
+use Sil\DevPortal\components\Http\ClientG5 as HttpClient;
+
+class IntegrationTest extends \CTestCase
+{
+    public function testSystemCheck()
+    {
+        // Arrange:
+        $systemCheckUrl = \Yii::app()->createAbsoluteUrl('/site/system-check/');
+        $client = new HttpClient();
+
+        // Act:
+        $response = $client->request('GET', $systemCheckUrl);
+        
+        // Assert:
+        $this->assertSame('OK', $response->getBody(), sprintf(
+            "Unexpected response from system-check URL. Response headers:\n%s",
+            $response->getHeaders()
+        ));
+    }
+}
