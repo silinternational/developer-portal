9b028ab34da82fa16b0124a62bd235daed6b4e2b
Matt Henderson <matt_henderson@sil.org>
2016-11-08 16:30:39 -0500

    Increase query limit for apiaxle API for tests.

--- build/setup-apiaxle.sh
+++ build/setup-apiaxle.sh
@@ -2,5 +2,5 @@
 
 # Setup ApiAxle with dev key/secret
 curl -X POST -H 'Content-type: application/json' 'http://api/v1/api/apiaxle' -d '{"endPoint":"api", "endPointTimeout": 5}'
-curl -X POST -H 'Content-type: application/json' 'http://api/v1/key/developer-portal-dev-key' -d '{"sharedSecret":"developer-portal-dev-secret","qps":1000,"qpd":10000}'
+curl -X POST -H 'Content-type: application/json' 'http://api/v1/key/developer-portal-dev-key' -d '{"sharedSecret":"developer-portal-dev-secret","qps":10000,"qpd":100000}'
 curl -X PUT 'http://api/v1/api/apiaxle/linkkey/developer-portal-dev-key'
