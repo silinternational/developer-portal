83ed8c8b8c7bac99767f0d78b4c9013ec9cd125b
Matt Henderson <matt_henderson@sil.org>
2016-11-09 10:12:28 -0500

    Include API internal endpoint in notification email to super admins.

--- application/protected/views/mail/api-added.php
+++ application/protected/views/mail/api-added.php
@@ -3,17 +3,17 @@
 /* @var $api \Sil\DevPortal\models\Api */
 ?>
 <p>A new API has been added to the API Developer Portal. </p>
-<p><?php echo sprintf(
-    '%s%s added an API named "%s". <a href="%s">Click here</a> for more '
+<p><?= sprintf(
+    '%s%s added an API named "%s" (%s). <a href="%s">Click here</a> for more '
     . 'information.',
     CHtml::encode($addedByUser ? $addedByUser->display_name : 'Someone'),
-    ($addedByUser ? ' (' . $addedByUser->email . ')' : ''),
+    CHtml::encode($addedByUser ? ' (' . $addedByUser->email . ')' : ''),
     CHtml::encode($api->display_name),
+    CHtml::encode($api->getInternalApiEndpoint()),
     \Yii::app()->createAbsoluteUrl('/api/details/', array(
         'code' => $api->code,
     ))
-); ?>
-</p>
+); ?></p>
 <p>
     API Added <?php echo date(Yii::app()->params['friendlyDateFormat']); ?>
 </p>
