cbb683dbeaa848a2be41d297255002731b97ecc0
Matt Henderson <matt_henderson@sil.org>
2016-11-18 10:41:14 -0500

    Increase signature time window for apiaxle API.

--- application/run-tests.sh
+++ application/run-tests.sh
@@ -13,7 +13,7 @@ echo -e "Migrations should be done. Ran for $TIME seconds.\n\n"
 
 # Setup ApiAxle with dev key/secret
 echo -e "Creating api...\n\n"
-curl -X POST -H 'Content-type: application/json' 'http://api/v1/api/apiaxle' -d '{"endPoint":"api", "endPointTimeout": 5}'
+curl -X POST -H 'Content-type: application/json' 'http://api/v1/api/apiaxle' -d '{"endPoint":"api", "endPointTimeout": 5, "tokenSkewProtectionCount": 5}'
 echo -e "Creating key...\n\n"
 curl -X POST -H 'Content-type: application/json' 'http://api/v1/key/developer-portal-dev-key' -d '{"sharedSecret":"developer-portal-dev-secret","qps":10000,"qpd":100000}'
 echo -e "Linking key to api...\n\n"
