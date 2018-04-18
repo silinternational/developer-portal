155a85db0afbcf837134a8de332fbcdac2995454
Matt Henderson <matt_henderson@sil.org>
2018-04-18 16:54:00 -0400

    Make the website navbar match the THEME_COLOR (if set)

--- application/run.sh
+++ application/run.sh
@@ -14,6 +14,11 @@ fi
 
 chown -R www-data:www-data /data/protected/runtime /data/public/assets
 
+# If a THEME_COLOR was provided, use that for the website navbar.
+if [[ "x" != "x$THEME_COLOR" ]]; then
+    sed -i /data/public/css/styles.css -e "s/0068a6/${THEME_COLOR}/"
+fi
+
 mkdir -p -v /data/vendor/simplesamlphp/simplesamlphp/cert
 cp /tmp/ssp-overrides/cert/* /data/vendor/simplesamlphp/simplesamlphp/cert
 cp /tmp/ssp-overrides/config/* /data/vendor/simplesamlphp/simplesamlphp/config
