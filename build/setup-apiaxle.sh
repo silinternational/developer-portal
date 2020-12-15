a0bd1113cd849fdfe2f6abf793684ac539016b82
Phillip Shipley <phillip.shipley@gmail.com>
2020-12-15 09:35:54 -0500

    Move ApiAxle env vars to common.env and reuse env vars in setup script

--- build/setup-apiaxle.sh
+++ build/setup-apiaxle.sh
@@ -4,5 +4,5 @@ sleep 10
 
 # Setup ApiAxle with dev key/secret
 curl -X POST -H 'Content-type: application/json' 'http://api/v1/api/apiaxle' -d '{"endPoint":"api", "endPointTimeout": 5, "tokenSkewProtectionCount": 5}'
-curl -X POST -H 'Content-type: application/json' 'http://api/v1/key/developer-portal-dev-key' -d '{"sharedSecret":"developer-portal-dev-secret","qps":10000,"qpd":100000}'
-curl -X PUT 'http://api/v1/api/apiaxle/linkkey/developer-portal-dev-key'
+curl -X POST -H 'Content-type: application/json' "http://api/v1/key/${APIAXLE_KEY}" -d '{"sharedSecret":"'"${APIAXLE_SECRET}"'","qps":10000,"qpd":100000}'
+curl -X PUT "http://api/v1/api/apiaxle/linkkey/${APIAXLE_KEY}"
