de7f67b7016806303360ccade07880278f158019
Matt Henderson <matt_henderson@sil.org>
2021-04-08 17:57:11 -0400

    Also handle SSP override files in that separate script

--- application/run.sh
+++ application/run.sh
@@ -10,11 +10,6 @@ if [[ "x" != "x$THEME_COLOR" ]]; then
     sed -i /data/public/css/styles.css -e "s/0068a6/${THEME_COLOR}/"
 fi
 
-mkdir -p -v /data/vendor/simplesamlphp/simplesamlphp/cert
-cp /tmp/ssp-overrides/cert/* /data/vendor/simplesamlphp/simplesamlphp/cert
-cp /tmp/ssp-overrides/config/* /data/vendor/simplesamlphp/simplesamlphp/config
-cp /tmp/ssp-overrides/metadata/* /data/vendor/simplesamlphp/simplesamlphp/metadata
-
 # Run database migrations (exiting with an error if they fail)
 runny /data/protected/yiic migrate --interactive=0
 
--- build/install-deps-and-ssp-overrides.sh
+++ build/install-deps-and-ssp-overrides.sh
@@ -5,3 +5,9 @@ set -e
 # Install composer dependencies
 cd /data
 composer install --no-dev --no-scripts --optimize-autoloader --no-interaction
+
+# Copy the SSP override files into place
+mkdir -p -v /data/vendor/simplesamlphp/simplesamlphp/cert
+cp /tmp/ssp-overrides/cert/* /data/vendor/simplesamlphp/simplesamlphp/cert
+cp /tmp/ssp-overrides/config/* /data/vendor/simplesamlphp/simplesamlphp/config
+cp /tmp/ssp-overrides/metadata/* /data/vendor/simplesamlphp/simplesamlphp/metadata
