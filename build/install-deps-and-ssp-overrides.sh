728ffc330aa3a857631b3efe7d666446d16e96d6
Matt Henderson <matt_henderson@sil.org>
2021-04-09 17:02:13 -0400

    Let script continue even if `/tmp/ssp-overrides/cert/` is absent or empty

--- build/install-deps-and-ssp-overrides.sh
+++ build/install-deps-and-ssp-overrides.sh
@@ -8,7 +8,7 @@ composer install --no-dev --no-scripts --optimize-autoloader --no-interaction
 
 # Copy the SSP override files into place
 mkdir -p -v /data/vendor/simplesamlphp/simplesamlphp/cert
-cp /tmp/ssp-overrides/cert/* /data/vendor/simplesamlphp/simplesamlphp/cert
+find /tmp/ssp-overrides/ -path '/tmp/ssp-overrides/cert/*' -exec cp {} /data/vendor/simplesamlphp/simplesamlphp/cert \;
 cp /tmp/ssp-overrides/config/* /data/vendor/simplesamlphp/simplesamlphp/config
 cp /tmp/ssp-overrides/metadata/* /data/vendor/simplesamlphp/simplesamlphp/metadata
 rm -rf /tmp/ssp-overrides
